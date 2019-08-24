e68da3530a4eb4ce7e89255e98d6a83b4f02b8cc
Widdiful <Widdiful@users.noreply.github.com>
2019-08-25 00:03:21 +0100

    Very basic combat, console only

--- MerchantsQuest/Assets/Scripts/Combat/CombatManager.cs
+++ MerchantsQuest/Assets/Scripts/Combat/CombatManager.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public static CombatManager instance;
+    public List<StatsBase> playerTeam, enemyTeam, turnOrder = new List<StatsBase>();
+    private int turnIndex;
+    private bool battleEnded;
+
+    private void Awake() {
+        instance = this;
+
+        // add all from players and enemies
+        // sort by agility stat
+
+        turnOrder.Add(playerTeam[0]);
+        turnOrder.Add(enemyTeam[0]);
+        turnOrder.Add(enemyTeam[1]);
+
+        NextTurn();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void NextTurn() {
+        if (!battleEnded) {
+            if (enemyTeam.Count > 0 && playerTeam.Count > 0) {
+
+                turnIndex = (turnIndex + 1) % (playerTeam.Count + enemyTeam.Count);
+
+                if (!turnOrder[turnIndex].isDead) {
+                    turnOrder[turnIndex].GetCommand();
+                }
+
+            }
+            else if (enemyTeam.Count <= 0) {
+                print("victory!");
+                battleEnded = true;
+            }
+            else if (playerTeam.Count <= 0) {
+                print("failure!");
+                battleEnded = true;
+            }
+        }
     }
+
+    public void Attack(StatsBase attacker, StatsBase target, int damage, bool ignoreDefence) {
+        int damageTaken = target.TakeDamage(damage, ignoreDefence);
+
+        print(attacker.characterName + " attacked " + target.characterName + " for " + damageTaken);
+
+        NextTurn();
+    }
+
 }
---
+++ MerchantsQuest/Assets/Scripts/Combat/StatsBase.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StatsBase
+{
+    public string characterName;
+    public int level, hitPoints, magicPoints, attack, defence, agility;
+    [Range(0, 1)]
+    public float critChance;
+    public bool isDead, isEnemy;
+
+    public int TakeDamage(int damage, bool ignoreDefence) {
+        if (!isDead) {
+            // Adjust damage based on defence stat
+            int defenceAdjusted = defence;
+            if (defence <= 0 || ignoreDefence) {
+                defenceAdjusted = 1;
+            }
+            int damageToTake = damage / defenceAdjusted;
+
+            // Deal damage, maybe die
+            hitPoints -= damageToTake;
+            if (hitPoints <= 0) {
+                hitPoints = 0;
+                Kill();
+            }
+
+            return damageToTake;
+        }
+
+        return 0;
+    }
+
+    public void Kill() {
+        isDead = true;
+
+        Debug.Log(characterName + " died!");
+
+        CombatManager.instance.turnOrder.Remove(this);
+        if (isEnemy) {
+            CombatManager.instance.enemyTeam.Remove(this);
+        }
+        else {
+            CombatManager.instance.playerTeam.Remove(this);
+        }
+    }
+
+    public virtual void GetCommand() {
+        if (isEnemy)
+            Attack(CombatManager.instance.playerTeam[0], attack);
+        else
+            Attack(CombatManager.instance.enemyTeam[0], attack);
+    }
+
+    public void Attack(StatsBase target, int damage) {
+        bool isCrit = false;
+        if (Random.Range(0f, 1f) <= critChance) {
+            isCrit = true;
+        }
+
+        CombatManager.instance.Attack(this, target, damage, isCrit);
+    }
+}
