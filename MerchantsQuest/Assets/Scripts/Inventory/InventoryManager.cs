e2668a079925bd774ddcf5d4e3260d4e046770b4
Ethan Bruins <brudut93@gmail.com>
2019-08-24 22:39:52 +0100

    started on inventory

---
+++ MerchantsQuest/Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    private bool inventoryEnabled;
+    private int availableSlots;
+    private int totalSlots;
+
+
+
+
+}
---
+++ MerchantsQuest/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType { Spell, Weapon, Armor, Consumable }
+
+//If the item is a spell type then you auto learn and consume
+public struct Item
+{
+    public string name;
+    public string fakeDescription;
+    public float realStat;
+    public bool appraised;
+    public ItemType type;
+
+}
