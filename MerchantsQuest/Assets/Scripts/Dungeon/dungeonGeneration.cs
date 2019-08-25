aa420c9669564ceeeb191d37befa70e34e82df26
Olly Mills <kinglslbob@gmail.com>
2019-08-25 01:22:27 +0100

    rooms generate

--- MerchantsQuest/Assets/Scripts/Dungeon/dungeonGeneration.cs
+++ MerchantsQuest/Assets/Scripts/Dungeon/dungeonGeneration.cs
@@ -5,37 +5,92 @@ using UnityEngine.Tilemaps;
 //using UnityEngine.TilemapModule;
 public class dungeonGeneration : MonoBehaviour
 {
-    //Tilemap localMap;
     public Tilemap tilemap;
     public TileBase tile;
+    public List<TileBase> tileList;
     int xSize;
     int ySize;
+
+    int nRooms;
+
+    public int maxRoomSize, minRoomSize;
     // Start is called before the first frame update
     void Start()
     {
         //localMap = GetComponent<Tilemap>();
-        xSize = 10;
-        ySize = 10;
+        xSize = 100;
+        ySize = 100;
+
+        nRooms = 10;
         int[,] map = new int[xSize,ySize];
         
         for (int x = 0; x < map.GetUpperBound(0) ; x++) //Loop through the width of the map
         {
             for (int y = 0; y < map.GetUpperBound(1); y++) //Loop through the height of the map
             {
-                if(Random.Range(1,10)>6)
+                //if(Random.Range(1,10)>6)
+                //{
+                //    map[x,y] = 1;
+                //}
+                //else
+                //{
+                //    map[x,y]=0;
+                //}
+                // fill map with 0's so it's all dirt 
+                map[x,y] = 1;
+            }
+        }
+
+        List<rect> roomList = new List<rect>();
+        int numRooms = 0;
+        for (int i = 0; i < nRooms; i++)
+        {
+            int w = Random.Range(minRoomSize, maxRoomSize);
+            int h = Random.Range(minRoomSize, maxRoomSize);
+
+            int x = Random.Range(1, xSize - w - 1);
+            int y = Random.Range(1, ySize - h - 1);
+
+            rect newRoom = new rect(x,y,h,w);
+
+            if(numRooms != 0)
+            {
+                for (int j = 0; j < roomList.Count; j++)
                 {
-                    map[x,y] = 1;
+                    if(newRoom.intersect(roomList[j]))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            createRoom(newRoom, map);
+            Vector2Int newCenter = newRoom.center();
+
+            if(numRooms == 0)
+            {
+
+            }
+            else
+            {
+                Vector2Int lastCenter = roomList[numRooms-1].center();
+
+                if(Random.Range(0,1)>0)
+                {
+                    createHTunnel(lastCenter.x, newCenter.x, lastCenter.y, map);
+                    createVTunnel(lastCenter.y, newCenter.y, lastCenter.x, map);
                 }
                 else
                 {
-                    map[x,y]=0;
+                    createVTunnel(lastCenter.y, newCenter.y, lastCenter.x, map);
+                    createHTunnel(lastCenter.x, newCenter.x, lastCenter.y, map);
                 }
-                //if (map[x, y] == 1) // 1 = tile, 0 = no tile
-                //{
-                //    tilemap.SetTile(new Vector3Int(x, y, 0), tile); 
-                //}
             }
+
+            roomList.Add(newRoom);
+            numRooms++;
         }
+
 
 
         for (int x = 0; x < map.GetUpperBound(0) ; x++) //Loop through the width of the map
@@ -44,7 +99,11 @@ public class dungeonGeneration : MonoBehaviour
             {
                 if (map[x, y] == 1) // 1 = tile, 0 = no tile
                 {
-                    tilemap.SetTile(new Vector3Int(x, y, 0), tile); 
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tileList[0]); 
+                }
+                else
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tileList[1]);
                 }
             }
         }
@@ -64,4 +123,52 @@ public class dungeonGeneration : MonoBehaviour
     {
         
     }
+
+
+    int[,] createRoom(rect room, int[,] map)
+    {
+        for (int x = room.getX1(); x < room.getX2(); x++)
+        {
+            for (int y = room.getY1(); y < room.getY2(); y++)
+            {
+                map[x,y] = 0;
+            }
+        }
+
+        return map;
+    }
+
+
+    int[,] createHTunnel(int x1, int x2, int y, int[,] map)
+    {
+        // 
+        
+        for (int x = min(x1, x2); x < min(x1, x2); x++)
+        {
+            map[x,y] = 0;
+        }
+
+        return map;
+    }
+
+    int[,] createVTunnel(int y1, int y2, int x, int[,] map)
+    {
+        // 
+        
+        for (int y = min(y1, y2); y < min(y1, y2); y++)
+        {
+            map[x,y] = 0;
+        }
+
+        return map;
+    }
+
+    int min(int x1,int x2)
+    {
+	    if(x1 > x2)
+		    return x2;
+	    else
+		    return x1;
+    }
+
 }
---
+++ MerchantsQuest/Assets/Scripts/Dungeon/rect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rect
+{
+    int x1, x2, y1, y2;
+    public rect(int x, int y, int h, int w)
+    {
+        x1 = x;
+        y1 = y;
+        x2 = x + w;
+        y2 = y + h;
+    }
+
+    public Vector2Int center()
+    {
+        Vector2Int center = new Vector2Int();
+        center.x = (x1 + x2)/2;
+        center.y = (y1 + y2)/2;
+        return center;
+    }
+
+    public bool intersect(rect other)
+    {
+        return( x1<= other.getX2() && x2 >=other.getX1() && y1 <= other.getY2() && y2 >= other.getY1() );
+    }
+
+    public int getX1()
+    {
+        return x1;
+    }
+
+    public int getX2()
+    {
+        return x2;
+    }
+
+    public int getY1()
+    {
+        return y1;
+    }
+    
+    public int getY2()
+    {
+        return y2;
+    }
+}
