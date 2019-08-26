cac09fcaaf1223d7ccdf9f2f604134fce15b9b46
Widdiful <Widdiful@users.noreply.github.com>
2019-08-26 02:38:13 +0100

    Background stuff

--- MerchantsQuest/Assets/Sprites/AnimatedBackground.cs
+++ MerchantsQuest/Assets/Sprites/AnimatedBackground.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class AnimatedBackground : MonoBehaviour
 {
     public RectTransform rect;
+    public Animator anim;
     private float timer;
+
+    private void Awake() {
+        anim.SetInteger("anim", Random.Range(0, 4));
+    }
 
     void Update()
     {
         float newY = Mathf.Sin(timer) + 1.5f;
+        float newX = Mathf.Sin(timer / 2) + 1.5f;
         timer += Time.deltaTime;
-        rect.localScale = new Vector3(1, newY, 1);
+        rect.localScale = new Vector3(newX, newY, 1);
         rect.Rotate(new Vector3(0, 0, Time.deltaTime * 10));
     }
 }
