c312fcf9b5f28c58cc26fa1d209e7f02473b887e
Olly Mills <kinglslbob@gmail.com>
2019-08-31 02:58:25 +0100

    that one line of code lewis needed

--- MerchantsQuest/Assets/Scripts/Dungeon/dungeonGeneration.cs
+++ MerchantsQuest/Assets/Scripts/Dungeon/dungeonGeneration.cs
@@ -356,4 +356,10 @@ public class dungeonGeneration : MonoBehaviour
         currentFloorNumber = floorNumber;
     }
 
+
+    public void hideButtons()
+    {
+        buttonReference.SetActive(false);
+    }
+
 }
