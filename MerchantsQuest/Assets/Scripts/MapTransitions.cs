9daedccd0b1e651479596f374a7244479e1c3bf2
Widdiful <Widdiful@users.noreply.github.com>
2019-08-31 03:17:17 +0100

    Dungeon buttons now go away!!!

--- MerchantsQuest/Assets/Scripts/MapTransitions.cs
+++ MerchantsQuest/Assets/Scripts/MapTransitions.cs
@@ -63,4 +63,10 @@ public class MapTransitions : MonoBehaviour
             Transition(collision.transform);
         }
     }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Player") && leadsToDungeon) {
+            dungeon.hideButtons();
+        }
+    }
 }
